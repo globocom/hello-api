4fef083331f1ce34d36cb3d8fd5b7114617049f1
matheus.costa <matheus.costa@g.globo>
2021-02-25 13:22:01 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use actix_web::{get, web, App, HttpServer, HttpResponse, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!!!")
+}
+
+#[get("/healthcheck")]
+async fn healthcheck() -> impl Responder {
+    HttpResponse::Ok().body("I'm alive!")
+}
+
+pub fn init(config: &mut web::ServiceConfig) {
+    config.service(
+        web::scope("")
+            .service(index)
+            .service(healthcheck)
+    );
+
+}#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .configure(init)
+    })
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
+}
